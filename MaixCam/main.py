4cc41de248329f32092b2f68d08b3b0f99819070
CosmosMount <yzhang1150@gmail.com>
2025-09-21 14:33:16 +0800

    fix tiny error

--- MaixCam/main.py
+++ MaixCam/main.py
@@ -1,6 +1,6 @@
 from maix import time
-from MaixAPI.apriltagmap import AprilTagConfig
-from MaixAPI.uarttransmit import CMD_Packet, DataTransimit
+from dredge.apriltagmap import AprilTagConfig
+from dredge.uarttransmit import CMD_Packet, DataTransimit
 
 """
 Example usage: Users can refer to following lines for implementation.
